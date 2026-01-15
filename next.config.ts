0cc338ed664b704c3cd7b3fc72d3def421e38865
dejan006 <dejankovacevic717@gmail.com>
2026-01-15 17:00:33 +0100

    Final Frontend Commit

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,10 @@ const checkoutUrl = "http://83.228.192.104:18082";
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
+      
       { source: "/api/backend/:path*", destination: `${recipeUrl}/:path*` },
       { source: "/api/checkout/:path*", destination: `${checkoutUrl}/:path*` },
+
     ];
   },
 };
